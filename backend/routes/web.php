dd1f6ec5319999b4d10d3f5e62b1db4f532756bb
Hack Dracko <hackdracko@MacBook-Air-de-Hack.local>
2020-09-29 00:10:40 -0500

    Adding apiv1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,13 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+$version = '/apiv1';
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/saveZipCodes', 'GasStationController@saveZipCodes');
-Route::get('/states', 'GasStationController@getStates');
-Route::get('/municipalities/{state}', 'GasStationController@getMunicipalities');
-Route::get('/zip-codes/{state}/{municipality}', 'GasStationController@getZipCodes');
-Route::get('/gasStations', 'GasStationController@index');
+Route::get($version . '/saveZipCodes', 'GasStationController@saveZipCodes');
+Route::get($version . '/states', 'GasStationController@getStates');
+Route::get($version . '/municipalities/{state}', 'GasStationController@getMunicipalities');
+Route::get($version . '/zip-codes/{state}/{municipality}', 'GasStationController@getZipCodes');
+Route::get($version . '/gasStations', 'GasStationController@index');
